8dd93995a006ec080b7da7576cdb28856afcbea6
fatmanurakbass <fatmanurakbas98@gmail.com>
2023-03-28 21:50:00 +0300

    city endpoint by id

--- src/main/java/com/example/mongocrud/CityController.java
+++ src/main/java/com/example/mongocrud/CityController.java
@@ -1,13 +1,17 @@
 package com.example.mongocrud;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api/cities")
 public class CityController {
@@ -17,4 +21,9 @@ public class CityController {
     public ResponseEntity<List<City>> getAllCities(){
         return new ResponseEntity<List<City>>(cityService.allCities(), HttpStatus.OK);
     }
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<City>> getCityById(@PathVariable ObjectId id){
+        return new ResponseEntity<Optional<City>>(cityService.city(id), HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/example/mongocrud/CityService.java
+++ src/main/java/com/example/mongocrud/CityService.java
@@ -1,9 +1,11 @@
 package com.example.mongocrud;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CityService {
@@ -11,5 +13,9 @@ public class CityService {
     private CityRepository cityRepository;
     public List<City> allCities(){
         return cityRepository.findAll();
+    }
+
+    public Optional<City> city(ObjectId id){
+        return cityRepository.findById(id);
     }
 }
